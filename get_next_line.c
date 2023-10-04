0279caf2f99716d6420d299fca7384e4bf46c625
Daniel Beres <dberes@c4r5p10.42vienna.com>
2023-10-04 15:09:25 +0200

    thursday after2

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,35 @@
 /*   By: dberes <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 11:36:35 by dberes            #+#    #+#             */
-/*   Updated: 2023/10/04 11:38:31 by dberes           ###   ########.fr       */
+/*   Updated: 2023/10/04 15:06:37 by dberes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
+#include <unistd.h>
 
 char	*get_next_line(int fd)
 {
+		char		*line;
+		int			cnt;
+		void		*buf;
+		int			i;
 
+		i = 0;
+		cnt = 10;
+		buf = (void *)malloc(cnt);
+		read(fd, buf, cnt);
+		line = (char *)buf;
+		read(fd, buf, cnt);
+		line = line + (char *)buf;
+		return (line);
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int	fd = open("try.txt", O_RDONLY); 
+	printf("%s", get_next_line(fd));
+	int close(int fd);
 }
